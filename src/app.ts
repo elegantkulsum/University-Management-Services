793945362949a55abcc272f7d0535f2651e2e7f7
KULSUM <you@example.com>
2023-06-11 18:04:57 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
-const app:Application = express()
-const port = 3000
-app.use(cors())
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+
+     app.use(cors())
 //parser
-app.use(express.json())//amader je reqest ta asbe express,json er maddome sei data ke parser korte cai
-app.use(express.urlencoded({extended:true}))// jodi url encoded thake tahoel amra true korbo
+app.use(express.json()) //amader je reqest ta asbe express,json er maddome sei data ke parser korte cai
+app.use(express.urlencoded({ extended: true })) // jodi url encoded thake tahoel amra true korbo
 
 //testing
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response:any) => {
   // res.status(200).json({
   //   "name":"kulsum"
   // })
   res.send('Testing! Working Successfully')
 })
 
-export default app;
+export default app
